775a1ce5822d25756c3c757409f65682442a710c
lucasoneves <casluhc@gmail.com>
2024-08-02 13:25:36 -0300

    First commit

---
+++ components/AppFooter/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <footer>
+      <span>Tiny projects</span>
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/AppHeader/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header>
+      <h1>Tiny projects</h1>
+    </header>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
